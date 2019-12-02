c8ce2919977a63349e253ecfc5475378bb6fcb6d
kalramanoj2002 <kalramanoj2002@gmail.com>
2019-12-02 15:00:04 -0500

    upgrade apidoc-core and fs-extra

--- test/simple/simpleTest.test.js
+++ test/simple/simpleTest.test.js
@@ -13,23 +13,12 @@ const options= {
     simulate: false,
     parse: false, // only parse and return the data, no file creation
     colorize: true,
-    markdown: true,
-
-    marked: {
-        gfm: true,
-        tables: true,
-        breaks: false,
-        pedantic: false,
-        sanitize: false,
-        smartLists: false,
-        smartypants: false
-    }
+    markdown: true
 }
 
-const expectedObject = {"swagger":"2.0","info":{"title":"apidoc-swagger","version":"0.2.3","description":"Convert api doc json to swagger json"},"paths":{"/user/id":{"get":{"tags":["User"],"consumes":["application/json"],"produces":["application/json"],"parameters":[{"name":"id","in":"path","required":true,"type":"number","description":"Users unique ID. "}],"responses":{"200":{"description":"successful operation","schema":{"type":"String","items":{"$ref":"#/definitions/GetUser"}}}}}}},"definitions":{"GetUser":{"properties":{"id":{"type":"number","description":"Users unique ID. "},"firstname":{"type":"string","description":"Firstname of the User. "},"lastname":{"type":"string","description":"Lastname of the User. "}},"required":["id","firstname","lastname"]}}};
+const expectedObject = {"swagger":"2.0","info":{"title":"apidoc-swagger","version":"0.2.3","description":"Convert api doc json to swagger json"},"paths":{"/user/id":{"get":{"tags":["User"],"consumes":["application/json"],"produces":["application/json"],"parameters":[{"name":"id","in":"path","required":true,"type":"number","description":"Users unique ID."}],"responses":{"200":{"description":"successful operation","schema":{"type":"String","items":{"$ref":"#/definitions/GetUser"}}}}}}},"definitions":{"GetUser":{"properties":{"id":{"type":"number","description":"Users unique ID."},"firstname":{"type":"string","description":"Firstname of the User."},"lastname":{"type":"string","description":"Lastname of the User."}},"required":["id","firstname","lastname"]}}};
 
 test('simple file should be transformed correctly', () => {
-    console.log(transformer);
     var transformedObj = transformer.createApidocSwagger(options);
     expect(JSON.parse(transformedObj.swaggerData)).toEqual(expectedObject);
 });
